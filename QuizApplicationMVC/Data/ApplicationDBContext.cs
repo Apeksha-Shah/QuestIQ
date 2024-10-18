ff7688f1d1f1c4e5b97eb24001b91c55e3a9c1f4
Apeksha-Shah <apekshashah029@gmail.com>
2024-10-18 13:46:25 +0530

    initial commit

---
+++ QuizApplicationMVC/Data/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuizApplicationMVC.Models;
+
+namespace QuizApplicationMVC.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
